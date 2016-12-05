403ff4a6c1e8aec105d553f64b092a775a7cc79a
root <root@localhost.localdomain>
2016-12-05 21:32:19 +0000

    wip

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -6,11 +6,15 @@ echo '##################################################################'
 
 
 yum install git -y || exit 1
+yum install epel-release -y || exit 1
+yum install vim -y || exit 1
 
+
+echo -e "\n\n\n" | ssh-keygen -P ""
 echo 'Host *' > ~/.ssh/config || exit 1
 echo '  StrictHostKeyChecking no' >> ~/.ssh/config || exit 1
 
-
+cd ~
 git clone https://github.com/Sher-Chowdhury/wordpress-codingbee.git || exit 1
 
 
@@ -26,7 +30,9 @@ PATH=$PATH:/opt/puppetlabs/bin || exit 1
 # http://unix.stackexchange.com/questions/22721/completely-restart-bash
 exec bash -l || exit 1
 
-puppet module install puppetlabs-wordpress_app --version 0.2.0 || exit 1
+puppet module install hunner-wordpress --version 1.0.0 || exit 1
 
+cd ~/wordpress-codingbee
 
+puppet apply site.pp
 
