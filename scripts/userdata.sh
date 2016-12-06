bc20224b7a24730cbcc0908c691103e2cf459caf
root <root@localhost.localdomain>
2016-12-06 21:05:40 +0000

    now install all the wordpress plugins

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -67,6 +67,30 @@ wp core install --path=/var/www/html --url=codingbee.net --title=Codingbee --adm
 
 chown -R apache:apache /var/www/html/
 
+wp plugin install 'coming-soon' --activate --path='/var/www/html/'
+wp plugin install 'custom-admin-bar' --activate --path='/var/www/html/'
+wp plugin install 'contact-form-7' --activate --path='/var/www/html/'
+wp plugin install 'custom-menu-wizard' --activate --path='/var/www/html/'
+wp plugin install 'disable-comments' --activate --path='/var/www/html/'
+wp plugin install 'display-widgets' --activate --path='/var/www/html/'
+wp plugin install 'duplicate-post' --activate --path='/var/www/html/'
+wp plugin install 'google-analytics-for-wordpress' --activate --path='/var/www/html/'
+wp plugin install 'save-grab' --activate --path='/var/www/html/'
+wp plugin install 'olevmedia-shortcodes' --activate --path='/var/www/html/'
+wp plugin install 'image-elevator' --activate --path='/var/www/html/'
+wp plugin install 'post-content-shortcodes' --activate --path='/var/www/html/'
+wp plugin install 'post-editor-buttons-fork' --activate --path='/var/www/html/'
+wp plugin install 'publish-view' --activate --path='/var/www/html/'
+wp plugin install 'recently-edited-content-widget' --activate --path='/var/www/html/'
+wp plugin install 'rel-nofollow-checkbox' --activate --path='/var/www/html/'
+wp plugin install 'search-filter' --activate --path='/var/www/html/'
+wp plugin install 'simple-custom-css' --activate --path='/var/www/html/'
+wp plugin install 'syntaxhighlighter' --activate --path='/var/www/html/'
+wp plugin install 'table-of-contents-plus' --activate --path='/var/www/html/'
+wp plugin install 'tablepress' --activate --path='/var/www/html/'
+wp plugin install 'wp-github-gist' --activate --path='/var/www/html/'
+
+
 
 
 
