4fa046c75f10e85b440b6d27ed630eb8d5fd6e6a
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-01-21 18:56:56 +0000

    bf

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -141,7 +141,9 @@ echo "db_username is ${db_username}"
 echo "db_password is ${db_password}"
 query="CREATE USER ${db_username}@'localhost' IDENTIFIED BY '${db_password}';"
 echo $query
-mysql --user="root" --execute="$query" || exit 1
+eval mysql --user="root" --execute="$query" 
+
+exit 1
 
 # This creates new db
 mysql -u root -e "CREATE DATABASE ${wp_db_name}" || { echo "ERROR: line ${LINENO}: failed to create DB"; exit 1; }
