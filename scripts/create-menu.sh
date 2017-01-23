4c6ab57ba9cc45425ab445e316a8ffa65863d877
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-01-23 21:00:29 +0000

    wip

--- scripts/create-menu.sh
+++ scripts/create-menu.sh
@@ -11,20 +11,35 @@ for csvfile in `ls /root/downloads/menus`; do
     echo "about to process: $line"
     post_title=`echo ${line} | cut -d',' -f1`
     parent_post_title=`echo ${line} | cut -d',' -f2`
+    menu_label=`echo ${line} | cut -d',' -f3`
 
     echo "The post title is ${post_title}"
     echo "The parent post title is ${parent_post_title}"
+    echo "The menu item label is ${menu_label}"
 
-    if [ ${parent_post_title} == 'null' ] ; then
-       echo "no parent"
-       post_id=`wp post list --path=/var/www/html --fields=ID,post_title | grep "$post_title" | awk '{print $2;}'`
+
+    if [[ ${parent_post_title} == 'null' && ${menu_label} == 'null' ]] ; then
+       post_id=`wp post list --path=/var/www/html --fields=ID,post_title | grep "${post_title}" | awk '{print $2;}'`
        echo "the post_id is ${post_id}"
        echo "about to run: wp menu item add-post ${menu_title} ${post_id} --path=/var/www/html"
        wp menu item add-post ${menu_title} ${post_id} --path=/var/www/html
-    else
-       echo "parent found"
     fi
 
+
+    if [[ ${parent_post_title} == 'null' && ${menu_label} != 'null' ]] ; then
+       post_id=`wp post list --path=/var/www/html --fields=ID,post_title | grep "${post_title}" | awk '{print $2;}'`
+       echo "the post_id is ${post_id}"
+       echo "about to run: wp menu item add-post ${menu_title} ${post_id} --title=${menu_label} --path=/var/www/html"
+       wp menu item add-post ${menu_title} ${post_id} --path=/var/www/html
+    fi
+
+    if [[ ${parent_post_title} != 'null' && ${menu_label} != 'null' ]] ; then
+       post_id=`wp post list --path=/var/www/html --fields=ID,post_title | grep "${post_title}" | awk '{print $2;}'`
+       parent_post_id=`wp post list --path=/var/www/html --fields=ID,post_title | grep "${parent_post_title}" | awk '{print $2;}'`
+       echo "the post_id is ${post_id}"
+       echo "about to run: wp menu item add-post ${menu_title} ${post_id} --title=${menu_label} --parent-id=${parent_post_id} --path=/var/www/html"
+       wp menu item add-post ${menu_title} ${post_id} --path=/var/www/html
+    fi
 
 
   done
