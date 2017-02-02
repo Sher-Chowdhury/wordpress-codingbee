bf44030f4798da73a53797fa50d3e2bfbc68d8d2
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-02-02 23:01:02 +0000

    wip

--- scripts/import_posts.rb
+++ scripts/import_posts.rb
@@ -1,1 +1,18 @@
 require ‘selenium-webdriver’
+
+driver = Selenium::WebDriver.for :firefox
+driver.navigate.to "http://codingbee.net/wp-login.php"
+
+user_login_element = driver.find_element(:id, 'user_login')
+
+user_login_element.send_keys 'sher'
+
+
+user_password_element = driver.find_element(:id, 'user_pass')
+
+
+user_password_element.send_keys 'admin'
+user_password_element.submit
+
+
+
