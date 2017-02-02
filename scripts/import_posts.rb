d1df1951bf50a0a860cc6322e036bf731824716f
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-02-02 23:30:05 +0000

    wip

--- scripts/import_posts.rb
+++ scripts/import_posts.rb
@@ -1,4 +1,4 @@
-require ‘selenium-webdriver’
+require 'selenium-webdriver'
 
 driver = Selenium::WebDriver.for :firefox
 driver.navigate.to "http://codingbee.net/wp-login.php"
@@ -11,8 +11,12 @@ user_login_element.send_keys 'sher'
 user_password_element = driver.find_element(:id, 'user_pass')
 
 
-user_password_element.send_keys 'admin'
+user_password_element.send_keys 'password'
 user_password_element.submit
 
 
+sleep(10)
+
+
+driver.navigate.to "http://codingbee.net/wp-admin/admin.php?page=pmxi-admin-settings"
 
