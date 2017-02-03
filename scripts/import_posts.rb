b04e6b2a42b4d05972df4e1ab415982d75f432a1
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-02-03 22:53:18 +0000

    wip

--- scripts/import_posts.rb
+++ scripts/import_posts.rb
@@ -15,8 +15,16 @@ user_password_element.send_keys 'password'
 user_password_element.submit
 
 
-sleep(10)
+sleep(5)
 
 
 driver.navigate.to "http://codingbee.net/wp-admin/admin.php?page=pmxi-admin-settings"
+
+upload_field = driver.find_element(:name, "template_file")
+
+upload_field.send_keys '/Users/schowdhury/Dropbox/codingbee/wp-all-import-exports/import-and-export-plugin-templates/import-templates/import-codingbee-posts-template.txt'
+
+driver.find_element(:name, "import_templates").click
+
+driver.navigate.to "http://codingbee.net/wp-admin/admin.php?page=pmxi-admin-import"
 
