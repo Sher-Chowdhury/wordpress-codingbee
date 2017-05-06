3ee5117ea6cea3f0a9f12dc88a1cf580fcaec404
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-05-06 19:29:18 +0100

    wip

--- scripts/userdata-freshbuild.sh
+++ scripts/userdata-freshbuild.sh
@@ -194,9 +194,9 @@ sed -i "s/USERNAME/$db_username/g" /var/www/html/content_correction_fix.php
 sed -i "s/PASSWORD/$db_password/g" /var/www/html/content_correction_fix.php
 sed -i "s/DATABASENAME/$wp_db_name/g" /var/www/html/content_correction_fix.php
 
-cp /var/www/html/bundle/codingbee-posts-exports.xml /var/www/html/
 cd /var/www/html/
 unzip /var/www/html/codingbee-posts-exports.zip
+cp /var/www/html/bundle/codingbee-posts-exports.xml /var/www/html/
 chown -R apache:apache /var/www/html/
 
 curl http://localhost/content_correction_fix.php
