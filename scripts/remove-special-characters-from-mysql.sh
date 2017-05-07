cb8e067f28da43c2a0a958b7e39566d5b37c227b
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-05-07 12:01:23 +0100

    wip

--- scripts/remove-special-characters-from-mysql.sh
+++ scripts/remove-special-characters-from-mysql.sh
@@ -2,7 +2,16 @@
 
 wp_db_name=${1}
 
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = replace(post_content, "â””â”€â”¬", "└─┬");'
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â””â”€â”€", "└──");'
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â”œâ”€â”€", "├──");'
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â”œâ”€", "├─");'
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â””â”€", "└─");'
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = replace(post_content, "â”‚", "│");'
 mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = replace(post_content, "Â", " ");'
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = replace(post_content, "â€", "”");'
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = replace(post_content, "â€", "-");'
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = replace(post_content, "Ö˜", "X");'
 mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â€œ", "“");'
 mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â€™", "’");'
 mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â€˜", "‘");'
@@ -10,3 +19,4 @@ mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post
 mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â€“", "-");'
 mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â€¢", "-");'
 mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â€¦", "…");'
+mysql -u root -D $wp_db_name -e 'UPDATE wp_posts SET post_content = REPLACE(post_content, "â€£", "‣");'
