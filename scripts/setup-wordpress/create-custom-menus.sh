ecf722ac37f5ab852b7d67fedfdf44ed916bdee5
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-05-23 23:55:03 +0100

    wip

--- scripts/setup-wordpress/create-custom-menus.sh
+++ scripts/setup-wordpress/create-custom-menus.sh
@@ -12,7 +12,7 @@ echo '##################################################################'
 
 #wp sidebar list --path=/var/www/html/
 wp widget add nav_menu left 1 --title="RHCSA" --path=/var/www/html/
-
+dropdown_menu_html_id=$(wp widget list left --path=/var/www/html/ --fields=id,options --format=csv | grep -i rhcsa | cut -d',' -f1)
 
 echo '##################################################################'
 echo '############## About to create csharp custom menu ################'
