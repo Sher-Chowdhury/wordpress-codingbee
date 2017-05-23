fe32e8eb38aa7df289e880c48723afbb19ab73de
Sher Chowdhury <sher.chowdhury@gmail.com>
2017-05-23 21:26:31 +0100

    selenium sample script now working

--- scripts/selenium-scripts/sample-wordpress-login-script.rb
+++ scripts/selenium-scripts/sample-wordpress-login-script.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+
 # pre-reqs
 # need to make sure you have the following installed:
 # - selenium ruby gem
@@ -7,7 +8,10 @@
 
 
 # you can run this script by doing the following
-# load './selenium.rb'
+# ./sample-wordpress-login-script.rb        
+# https://stackoverflow.com/questions/13112245/ruby-how-to-load-a-file-into-interactive-ruby-console-irb
+
+ 
 require 'selenium-webdriver'
 driver = Selenium::WebDriver.for :firefox
 driver.navigate.to "http://codingbee.net/wp-login.php"
@@ -15,6 +19,8 @@ user_login_element = driver.find_element(:id, 'user_login')
 user_login_element.send_keys "sher"
 user_password_element = driver.find_element(:id, 'user_pass')
 user_password_element.send_keys "password"
+sleep(5)
 driver.find_element(:id, "wp-submit").click
+sleep(5)
 driver.navigate.to "http://codingbee.net/wp-admin/widgets.php"
 # driver.quit
